ca0cda8755b7d1c585e197a0bc156cfbe931a802
Gitjamjam <jajajadra@gmail.com>
2019-10-14 10:48:16 +0900

    update 2019

---
+++ TextBook-Start-20181014/src/c2/leapyear2.py
@@ -0,0 +1,16 @@
+year = int(input("西暦何年 ? "))
+
+is_leap = False
+if year % 400 == 0:
+    is_leap = True
+elif year % 100 == 0:
+    is_leap = False
+elif year % 4 == 0:
+    is_leap = True
+else:
+    is_leap = False
+
+if is_leap:
+    print("うるう年です")
+else:
+    print("平年です")
---
+++ TextBook-Start-20181014/src/c2/pass.py
@@ -0,0 +1,9 @@
+n = 5
+if n == 3:
+    pass
+else:
+    print(n)
+
+if n == 3: print("true desu")
+if n == 5: print("5")
+
